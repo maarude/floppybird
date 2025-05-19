fa50943dcd83ed87c39d78d7f24bc404bddb9bc6
maarude <moa.3mri@gmail.com>
2025-05-20 01:38:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var states = Object.freeze({
 
 var currentstate;
 
-var gravity = 0.25;
+var gravity = 0.15;
 var velocity = 0;
 var position = 180;
 var rotation = 0;
@@ -25,7 +25,7 @@ var pipes = new Array();
 var replayclickable = false;
 
 //sounds
-var volume = 30;
+var volume = 10;
 var soundJump = new buzz.sound("assets/sounds/sfx_wing.ogg");
 var soundScore = new buzz.sound("assets/sounds/sfx_point.ogg");
 var soundHit = new buzz.sound("assets/sounds/sfx_hit.ogg");
